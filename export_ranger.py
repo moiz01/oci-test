98001d125efe2d7f658de1b575d399c1fd7651aa
moiz01 <moiz.cooltech@gmail.com>
2024-09-09 11:10:40 +0530

    Update export_ranger.py

--- export_ranger.py
+++ export_ranger.py
@@ -6,7 +6,7 @@ def execute(ranger_policy_helper):
         logger.info('Testing Ranger export-import functions')
         
         ranger_config_dict = {
-        "output_folder_path": "/tmp",  # Mandatory param, exports output file to /tmp folder
+        "output_folder_path": "/tmp/ranger_restore",  # Mandatory param, exports output file to /tmp folder
         }
         
         ranger_policy_helper.export_ranger_policies(ranger_config_dict)
